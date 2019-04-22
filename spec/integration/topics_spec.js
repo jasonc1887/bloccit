7464df9dd487a72c834ed183c1f3d7bba89b47b1
Jason Chavez <jasonc1887@gmail.com>
2019-04-22 11:08:39 -0600

    bloccit assignment 10: completed

--- spec/integration/topics_spec.js
+++ spec/integration/topics_spec.js
@@ -61,6 +61,29 @@ describe("routes : topics", () => {
       }
     };
 
+    it("should not create a new topic that fails validations", (done) => {
+      const options = {
+        url: `${base}create`,
+        form: {
+          title: "a",
+          description: "b"
+        }
+      };
+      request.post(options,
+        (err, res, body) => {
+          Topic.findOne({where: {title: "a"}})
+          .then((topic) => {
+            expect(topic).toBeNull();
+            done();
+          })
+          .catch((err) => {
+            console.log(err);
+            done();
+          });
+        }
+      );
+    });
+
     it("should create a new topic and redirect", (done) => {
 
       request.post(options,
