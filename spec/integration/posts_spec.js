e6fb9b8cd62752fdeec387ec254d5b56a28d034c
Jason Chavez <jasonc1887@gmail.com>
2019-04-22 09:37:22 -0600

    bloccit checkpoint 10: completed

--- spec/integration/posts_spec.js
+++ spec/integration/posts_spec.js
@@ -167,26 +167,27 @@ describe("routes : posts", () => {
     });
 
     it("should update the post with the given values", (done) => {
-        const options = {
-          url: `${base}/${this.topic.id}/posts/${this.post.id}/update`,
-          form: {
-            title: "Snowman Building Competition"
-          }
-        };
-        request.post(options,
-          (err, res, body) => {
+      const options = {
+        url: `${base}/${this.topic.id}/posts/${this.post.id}/update`,
+        form: {
+          title: "Snowman Building Competition",
+          body: "I love watching them melt slowly."
+        }
+      };
+      request.post(options,
+        (err, res, body) => {
 
-          expect(err).toBeNull();
+        expect(err).toBeNull();
 
-          Post.findOne({
-            where: {id: this.post.id}
-          })
-          .then((post) => {
-            expect(post.title).toBe("Snowman Building Competition");
-            done();
-          });
+        Post.findOne({
+          where: {id: this.post.id}
+        })
+        .then((post) => {
+          expect(post.title).toBe("Snowman Building Competition");
+          done();
         });
-    });
+      });
+  });
 
   });
 });
