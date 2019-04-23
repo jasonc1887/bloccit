2b58814f6b852d22340a20b80dd0305cab87c3c7
Jason Chavez <jasonc1887@gmail.com>
2019-04-23 11:45:23 -0600

    bloccit checkpoint 11: completed

--- src/routes/validation.js
+++ src/routes/validation.js
@@ -1,4 +1,5 @@
 module.exports = {
+  
   validatePosts(req, res, next) {
 
     if(req.method === "POST") {
@@ -14,6 +15,24 @@ module.exports = {
 
       req.flash("error", errors);
       return res.redirect(303, req.headers.referer)
+    } else {
+      return next();
+    }
+  }, 
+
+  validateUsers(req, res, next) {
+    if(req.method === "POST") {
+
+      req.checkBody("email", "must be valid").isEmail();
+      req.checkBody("password", "must be at least 6 characters in length").isLength({min: 6})
+      req.checkBody("passwordConfirmation", "must match password provided").optional().matches(req.body.password);
+    }
+
+    const errors = req.validationErrors();
+
+    if (errors) {
+      req.flash("error", errors);
+      return res.redirect(req.headers.referer);
     } else {
       return next();
     }
